7bf372f0120ae4b70bce10e2319b7dbcfe8ec0c0
Anna <annarosewain9@gmail.com>
2025-01-06 13:53:44 +0000

    Added links to Navbar

--- components/layout/MainNav.tsx
+++ components/layout/MainNav.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ToggleMode from "./ToggleMode";
-
+import Link from "next/link";
 
 const MainNav = () => {
   return (
-    <nav>
+    <nav className="flex justify-between">
+      <Link href="/">
+        <h2>Anna-Rose Wain</h2>
+      </Link>
+      <Link href="work">WORK</Link>
+      <Link
+        href="mailto:annarosewain9@gmail.com"
+        className="menu-link contact-link"
+      >
+        <div className="link-text">CONTACT</div>
+      </Link>
       <ToggleMode />
     </nav>
   );
