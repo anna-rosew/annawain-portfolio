3e6094ec182982cbee3adf482ba77addc3da7b02
Anna <annarosewain9@gmail.com>
2025-01-07 09:22:21 +0000

    Edited styling of links

--- app/page.tsx
+++ app/page.tsx
@@ -40,30 +40,29 @@ export default function Home() {
           UK.
         </h1>
       </div>
-
-      <div className="p-[20px]">
-        <div className="relative items-center justify-center ">
-          <Image src={emoji} alt="Robot Gif" className=" h-[70px]" />
-          <Image src={arrow} alt="Arrow pointing down" className=" h-[150px]" />
-        </div>
-      </div>
-      <div className="flex flex-wrap sm:flex-col md:flex-row gap-4">
+      <div className="flex flex-wrap sm:flex-col md:flex-row gap-1">
         {detailsItems.map((item, index) => (
           <div
             key={index}
-            className="bg-gray-200 p-4 rounded-lg w-full md:w-auto"
+            className=" lg:p-4 px-2 py-2 rounded-lg w-full md:w-auto "
           >
-            <h2 className="text-lg mb-2 text-left">{item.title}</h2>
+            <h2 className="mb-1 text-left text-sm">{item.title}</h2>
             <Link
               href={item.link}
               target="_blank"
               rel="noopener noreferrer"
-              className=" underline text-left"
+              className="underline text-left"
             >
               {item.linkText}
             </Link>
           </div>
         ))}
+      </div>
+      <div className="p-[20px]">
+        <div className="relative items-center justify-center ">
+          <Image src={emoji} alt="Robot Gif" className=" h-[70px]" />
+          <Image src={arrow} alt="Arrow pointing down" className=" h-[150px]" />
+        </div>
       </div>
 
       <Marquee />
